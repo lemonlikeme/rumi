99dde2f318a625a9bbefe99c9b6b54217d30cbf0
Lemon <lemonkarlmaverick@gmail.com>
2025-03-23 18:53:50 +0800

    codemagic

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'main_screen.dart'; // Your login screen
 
 void main() async {
   runApp(const MyApp());
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(); // Initialize Firebase
   runApp(MyApp());
 }
 
